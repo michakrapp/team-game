651f7de69c6fa75734c1f6d860042c6da098d375
Micha Krapp <github@michakrapp.com>
2023-02-27 19:43:58 +0100

    add points to name

--- src/Board/Cells/cells.ts
+++ src/Board/Cells/cells.ts
@@ -12,25 +12,25 @@ const cellStart:CellDataProps = {
 
 const cellSpecial:CellDataProps = {
     'id': 1,
-    'name': 'Special',
+    'name': 'Special (1)',
     'value': 'special'
 }
 
 const cellRed:CellDataProps = {
     'id': 2,
-    'name': 'Red',
+    'name': 'Red (2)',
     'value': 'red',
 }
 
 const cellBlue:CellDataProps = {
     'id': 3,
-    'name': 'Blue',
+    'name': 'Blue (3)',
     'value': 'blue',
 }
 
 const cellGreen:CellDataProps = {
     'id': 4,
-    'name': 'Green',
+    'name': 'Green (4)',
     'value': 'green',
 }
 
